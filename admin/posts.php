216b6c8bb7c4ed789d1dca9d01fc167f375f5e99
GitWithMe <namitmatrix@gmail.com>
2018-07-11 16:23:29 +0530

    this commit

--- admin/posts.php
+++ admin/posts.php
@@ -15,60 +15,28 @@
                          <small>Author</small>
                        </h1>
 
-                       <table class="table table-bordered table-hover">
-                           <thead>
-                               <tr>
-                                   <th>ID</th>
-                                   <th>Author</th>
-                                   <th>Title</th>
-                                   <th>Category</th>
-                                   <th>Status</th>
-                                   <th>Image</th>
-                                   <th>Tags</th>
-                                   <th>Comments</th>
-                                   <th>Date</th>
-                               </tr>
-                           </thead>
-                           <tbody>
+                       <?php 
 
-                    <?php 
-
-                   $query = "SELECT * FROM posts";
-                   $getpost = mysqli_query($connection,$query);
-                   if(!$getpost){
-                    die("Query Failed".mysqli_error($connection));
-                   }
-                   while($row = mysqli_fetch_assoc($getpost)){
-                    $post_id = $row['post_id'];
-                    $post_author = $row['post_author'];
-                    $post_title = $row['post_title'];
-                    $post_cat_id = $row['post_cat_id'];
-                    $post_status = $row['post_status'];
-                    $post_image = $row['post_image'];
-                    $post_tags = $row['post_tags'];
-                    $post_comments_count = $row['post_comments_count'];
-                    $post_date = $row['post_date'];
-
-                    echo "<tr><td>{$post_id}</td>
-                               <td>{$post_author}</td>
-                               <td>{$post_title}</td>
-                               <td>{$post_cat_id}</td>
-                               <td>{$post_status}</td>
-                               <td><img class='img-responsive' src='../images/{$post_image}'></td>
-                               <td>{$post_tags}</td>
-                               <td>{$post_comments_count}</td>
-                               <td>{$post_date}</td></tr>
-                    ";
-
-                   }
-         
-                    ?>
-                     
-
-                               
-                           </tbody>
-                       </table>
-                       
+                     if(isset($_GET['source'])){
+                        $source = $_GET['source'];
+                     }
+                     else{
+                        $source = '';
+                     }
+                     switch ($source) {
+                         case 'add_posts':
+                             include "includes/add_posts.php";
+                             break;
+                         case '76':
+                             echo "Nice 76";
+                             break;    
+                         
+                         default:
+                            include 'view_posts.php';
+                             break;
+                     }
+ 
+                       ?>
              </div>
                 </div>
                 <!-- /.row -->
